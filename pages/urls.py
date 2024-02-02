6595f03d26b87defc0c01e9416c36017ab15edb2
Rasul Kireev <me@rasulkireev.com>
2024-02-02 12:37:20 -0500

    form refactor

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, PrivacyView, ProductHuntView, SupportView, TosView
+from .views import HomeView, PrivacyView, SupportView, TosView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     # path("pricing", PricingView.as_view(), name="pricing"),
-    path("product-hunt", ProductHuntView.as_view(), name="product-hunt"),
+    # path("product-hunt", ProductHuntView.as_view(), name="product-hunt"),
     path("support", SupportView.as_view(), name="support"),
     path("privacy", PrivacyView.as_view(), name="privacy"),
     path("tos", TosView.as_view(), name="tos"),
